aaa3aa27c1e7096bc08985c404a96ebf5923b29a
Galina Stepanenko <sorihina_galina@mail.ru>
2024-01-16 22:07:01 +0300

    second commit

--- main.go
+++ main.go
@@ -32,6 +32,9 @@ func (t Training) distance() float64 {
 
 // meanSpeed возвращает среднюю скорость бега или ходьбы.
 func (t Training) meanSpeed() float64 {
+	if t.Duration.Hours() == 0 {
+		return 0
+	}
 	return t.distance() / t.Duration.Hours()
 }
 
